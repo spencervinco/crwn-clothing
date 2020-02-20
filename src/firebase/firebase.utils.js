a3b4a8ad29e6c404d1f8d6845484134ea356f55d
Spencer Vinco <spencer_vinco@anfcorp.com>
2020-02-20 16:59:41 -0500

    Adding sign up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,11 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app"; //pulling in firebase utility library.
+import "firebase/firestore"; //for database
+import "firebase/auth"; //for authentication
+
+//npm add firebase
+//manually add this file and reference in app.js
+//add below config from firebase dashboard
+//go into set up sign in method in authentication page on firebase website
 
 const config = {
   apiKey: "AIzaSyDDB_BCtSZsvWBZH1PNriOjucDzWG8TUNE",
@@ -13,14 +18,32 @@ const config = {
   measurementId: "G-E579RTJ0FL"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'});
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
-
