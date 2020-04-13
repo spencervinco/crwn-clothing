b079cf010d6e730c1fd3eb3ffe57e57a80d9fb52
Spencer Vinco <spencer_vinco@anfcorp.com>
2020-04-13 14:00:09 -0400

    my start at decrease item

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -9,6 +9,11 @@ export const addItemAction = item => ({
   payload: item
 });
 
+export const removeItemAction = item => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item
+});
+
 export const clearItemFromCart = item => ({
   type: CartActionTypes.CLEAR_ITEM_FROM_CART,
   payload: item
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,3 +16,11 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+}; 
