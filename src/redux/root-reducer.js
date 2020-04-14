10acc319c725fcbeefce7471797220986d90bb22
Spencer Vinco <spencerv@gmail.com>
2020-04-14 06:47:53 -0400

    adding redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,21 @@
 //npm install redux redux-logger react-redux
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import userReducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root', // at what point inside our reducer object do we want to start storing everything
+  storage,
+  whitelist: ['cart'] // string names of the reducers we want to store
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: CartReducer
 });
+
+
+export default persistReducer(persistConfig, rootReducer);
