56a101cedc9b5aa578e89a9ababd77e2a73a2567
Spencer Vinco <spencerv@gmail.com>
2020-04-15 15:40:40 -0400

    adding routing for category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+import CategoryPage from '../category/category.component';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = () => (
-  <div className="shop-page">
-    <CollectionsOverview />
-  </div>
-)
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+    </div>
+  )
+}
 
 export default ShopPage;
